2f099c1bc4479b5d60a17abf1126640b304a8fb2
Shani Henriques <shanihenriques@gmail.com>
2024-08-29 08:08:02 +0100

    echoing data without django

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
 from django.http import JsonResponse
+import json
 
-def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "django api response"})
+def api_home(request, *args, **kwargs): # request -> HttpRequest class instance from django
+
+    print(request.GET) # prints url query params
+    body = request.body # byte string of json
+    data = {}
+    try:
+        data = json.loads(body) # string of json data to python dict
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,5 +1,5 @@
 import requests
 
-endpoint = "http://localhost:8000/api"
-get_response = requests.get(endpoint)
+endpoint = "http://localhost:8000/api/"
+get_response = requests.get(endpoint, params={"hi": "you"}, json={"query": "hello world"})
 print(get_response.json()) 
