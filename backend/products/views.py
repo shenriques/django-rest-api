de184ec0b482eb6a1fa27d86a61e0e60f0233dfb
Shani Henriques <shanihenriques@gmail.com>
2024-08-30 15:40:35 +0100

    using the create api view

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serialisers import ProductSerialiser
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerialiser
+
+    # method for overriding object save / deletion behaviour
+    def perform_create(self, serializer):
+
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        # if user doesnt provide content for new object, set it to be the title
+        if content is None:
+            content = title
+
+        serializer.save(content=content)
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    # has to be serializer not serialiser!!!
+    serializer_class = ProductSerialiser
