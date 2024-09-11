6d5b28327d11d5b85463f4e8d09efd8a36bebb26
Shani Henriques <shanihenriques@gmail.com>
2024-09-11 16:26:30 +0100

    handle 'request blocked by cors' with django-cors-headers from requirements

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # third party api services
     'algoliasearch_django',
     # third party packages
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -60,6 +62,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
+CORS_URLS_REGEX = r"^/api/.*"
+
+CORS_ALLOWED_ORIGINS = []
+if DEBUG:
+    CORS_ALLOWED_ORIGINS += [
+        'http://localhost:8111',
+    ]
 
 TEMPLATES = [
     {
