78b3aff22a8e945df7617e1b1fc0c87cac62828b
Shani Henriques <shanihenriques@gmail.com>
2024-09-12 11:35:46 +0100

    listing product using jwt access

--- js_client/client.js
+++ js_client/client.js
@@ -3,6 +3,8 @@ const loginForm = document.getElementById('login-form');
 
 const baseEndpoint = "http://localhost:8000/api";
 
+const contentContainer = document.getElementById('content-container');
+
 if (loginForm) {
 
     // add event listener to the handler
@@ -34,10 +36,66 @@ function handleLogin(event) {
     .then(response => {
             return response.json()
     })
-    .then (x => {
-        console.log(x)
-    })
+    .then(
+        authData => { 
+            handleAuthData(authData, getProductList) // after authdata is set, list products
+        })
     .catch (err => {
         console.log('error', err)
+    })
+}
+
+function handleAuthData(authData, callback) {
+    // set these token
+    localStorage.setItem('access', authData.access)
+    localStorage.setItem('refresh', authData.refresh)
+
+    // call the function passed in (list products)
+    if (callback) {
+        callback()
+    }
+}
+
+function writeToContainer(data) {
+    if (contentContainer) {
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>"
+    }
+}
+
+function getFetchOptions(method, body) {
+    return {
+        // GET request if method undefined, otherwise method
+        method: method === null ? "GET" : method,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem('access')}`
+        },
+        body: body ? body : null
+    }
+}
+
+function isTokenNotValid(jsonData) {
+    if (jsonData.code && jsonData.code === "token_not_valid") {
+        // or run a refresh token fetch
+        alert("Please log in again")
+        return false
+    }
+    return true
+}
+
+function getProductList() {
+    const endpoint = `${baseEndpoint}/products/`
+    const options = getFetchOptions()
+
+    fetch(endpoint, options)
+    .then(response => {
+        console.log(response)
+        return response.json()
+    })
+    .then(data => {
+        const validData = isTokenNotValid(data) 
+        if (validData) {
+            writeToContainer(data)
+        }
     })
 }
